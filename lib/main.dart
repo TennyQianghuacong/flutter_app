aaa5ef8a2e1a9b055f1dbc7982befa4f0449aedc
TennyQianghuacong <dreamer.qianghc@gmail.com>
2019-07-02 14:34:28 +0800

    彩虹糖

--- lib/main.dart
+++ lib/main.dart
@@ -2,37 +2,50 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyAPP());
 
-class MyAPP extends StatelessWidget{
-
+class MyAPP extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Text Widget',
       home: Scaffold(
-        appBar: AppBar(title: Text('Text Widget'),),
-        body: Center(
-          child: Container(
-            child: Text('Hello world', style: TextStyle(fontSize: 40.0, color: Colors.white70), ),
-            alignment: Alignment.bottomRight,
-            width: 500,
-            height: 400,
-            padding: EdgeInsets.all(10.0),
-            margin: EdgeInsets.all(10.0),
-            decoration: BoxDecoration(
-              gradient: LinearGradient(colors: [Colors.pink,Colors.red,Colors.orange]),
-              border: Border.all(width: 3.0, color: Colors.redAccent),
-              borderRadius: BorderRadius.all(Radius.circular(3.0)),
-              boxShadow: [BoxShadow(
-                color: Color(0x66FF1744),
-                spreadRadius: 2.0,
-                blurRadius: 3.0,
-                offset: Offset(1.0, 1.0)
-              )]
+        appBar: AppBar(
+          title: Text('Text Widget'),
+          backgroundColor: Colors.orangeAccent,
+        ),
+        body: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            new Container(
+              width: 80.0,
+              color: Colors.red,
             ),
-          ),
+            new Container(
+              width: 80.0,
+              color: Colors.orange,
+            ),
+            new Container(
+              width: 80.0,
+              color: Colors.yellow,
+            ),
+            new Container(
+              width: 80.0,
+              color: Colors.green,
+            ),
+            new Container(
+              width: 80.0,
+              color: Colors.lightBlueAccent,
+            ),
+            new Container(
+              width: 80.0,
+              color: Colors.blue,
+            ),
+            new Container(
+              width: 80.0,
+              color: Colors.purple,
+            )
+          ],
         ),
       ),
     );
   }
-
 }
